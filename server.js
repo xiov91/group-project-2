0099e6922e0d0c3007074821f7d8143a7836c8d8
Miguel Soriano <originalsorianom@gmail.com>
2022-08-04 17:53:56 -0700

    update to file merge

--- server.js
+++ server.js
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
-const path = require('path');
-
-app.use(express.static(path.join(__dirname, 'public')));
-=======
 // server is setup to take routes, but routes aren't setup yet!
 // Once they're good, you can start the server with "npm start"
 // Just make sure the MySQL database is setup first ("mysql -u root -p", enter your password, then "source db/schema.sql")
@@ -23,4 +18,3 @@ sequelize.sync({ force: false }).then(() => {
 		console.log(`App listening on port http://localhost:${PORT}`);
 	});
 });
->>>>>>> 5418e3054c9636c47b6edef92aac44f301360b56
