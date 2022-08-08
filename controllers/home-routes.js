a1e1af63ca993b8475c46c92603dbd5c84d8f51a
xiov91 <99517717+xiov91@users.noreply.github.com>
2022-08-07 22:42:50 -0400

    login/logout complete + basic pages

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -35,6 +35,11 @@ router.get('/', (req, res) => {
 });
 
 router.get('/login', (req, res) => {
+    if (req.session.loggedIn) {
+        res.redirect('/');
+        return;
+    }
+
     res.render('login');
 });
 
--- server.js
+++ server.js
@@ -8,6 +8,20 @@ const routes = require("./controllers");
 const path = require("path");
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({});
+const session = require('express-session');
+
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+	secret: process.env.MYSECRET,
+	cookie: {},
+	resave: false,
+	saveUninitialized: true,
+	store: new SequelizeStore({
+		db: sequelize
+	})
+};
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -17,6 +31,7 @@ app.set('view engine', 'handlebars');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session(sess));
 
 app.use(routes);
 sequelize.sync({ force: false }).then(() => {
